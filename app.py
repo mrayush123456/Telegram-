187eb3b1712db6c467be8888f0771f7206274a80
mrayush123456 <yk7129388@gmail.com>
2024-12-20 10:25:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -106,5 +106,4 @@ def telegram_bot():
     return render_template_string(HTML_TEMPLATE)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-                          
+    app.run(host="0.0.0.0", port=5000, debug=True)
